dce2a303a5a67c15151ba3520d6e699fdd4b0a4a
Justin Klitgaard <jlke45@gmial.com>
2024-04-26 18:01:04 +0900

    Add logic to validate form

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const form = document.querySelector(".form");
 const output = document.querySelector(".output");
+const input = document.querySelector("#numInput");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const formData = new FormData(form);
   const numInput = formData.get("numInput");
   const base10 = main(numInput);
   output.textContent = base10;
+});
+
+input.addEventListener("input", (event) => {
+  const value = event.target.value;
+  const isBinary = value.match(/^[01]+$/);
+  if (!isBinary) {
+    console.log("no");
+    input.setCustomValidity("Enter 0s and 1s only.");
+  } else {
+    console.log("yes");
+    input.setCustomValidity("");
+  }
 });
 
 function main(str) {
